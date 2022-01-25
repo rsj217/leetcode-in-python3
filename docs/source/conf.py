e5aac904ea8ac68c03a9375ea9e95451914cb2a3
琤琮 <chengcong.mjm@autonavi.com>
2022-01-25 17:46:13 +0800

    thema

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,6 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -40,6 +39,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,13 +56,15 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
+# import sphinx_rtd_theme
 
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
+
+language = "zh_CN"
