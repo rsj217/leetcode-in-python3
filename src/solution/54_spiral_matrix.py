788a77c289fab1c95c5b4a3d1480ec2c95796b7b
rsj217 <chengcong.mjm@autonavi.com>
2022-02-02 18:59:45 +0800

    54

--- src/solution/54_spiral_matrix.py
+++ src/solution/54_spiral_matrix.py
@@ -13,31 +13,31 @@ class Solution:
         ans = []
 
         while 0 < num:
-            print(num)
             # l -> r
             for x in range(l, r + 1):
-                ans.append(matrix[t][x])
-                num -= 1
+                if num > 0:
+                    ans.append(matrix[t][x])
+                    num -= 1
             t += 1
-
-            print(num)
-
             # t -> b
             for y in range(t, b + 1):
-                ans.append(matrix[y][r])
-                num -= 1
+                if num > 0:
+                    ans.append(matrix[y][r])
+                    num -= 1
             r -= 1
 
             # l <- r
             for x in range(r, l - 1, -1):
-                ans.append(matrix[b][x])
-                num -= 1
+                if num > 0:
+                    ans.append(matrix[b][x])
+                    num -= 1
             b -= 1
 
             # t <- b
             for y in range(b, t - 1, -1):
-                ans.append(matrix[y][l])
-                num -= 1
+                if num > 0:
+                    ans.append(matrix[y][l])
+                    num -= 1
             l += 1
 
         return ans
@@ -47,9 +47,9 @@ class TestSolution(unittest.TestCase):
 
     def setUp(self):
         self.test_case = [
-            # ([[1]], [1]),
-            # ([[1, 2], [3, 4]], [1, 2, 4, 3]),
-            # ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+            ([[1]], [1]),
+            ([[1, 2], [3, 4]], [1, 2, 4, 3]),
+            ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
             ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]),
         ]
         self.s = Solution()
