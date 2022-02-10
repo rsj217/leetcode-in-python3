52769355a0b8ea3e5ecd78dbd2750e6b60ff4d0a
rsj217 <chengcong.mjm@autonavi.com>
2022-02-10 11:43:28 +0800

    26

--- src/solution/26_remove_duplicates_from_sorted_array.py
+++ src/solution/26_remove_duplicates_from_sorted_array.py
@@ -39,7 +39,7 @@ class Solution:
             return len(nums)
 
         j = 0
-        for i in range(0, len(nums)):
+        for i in range(1, len(nums)):
             if nums[j] < nums[i]:
                 nums[j + 1] = nums[i]
                 j += 1
@@ -52,6 +52,7 @@ class TestSolution(unittest.TestCase):
         self.test_case = [
             ([2, 2, 3, 3], 2),
             ([0, 1, 2, 2, 3, 4], 5),
+            ([0,0,1,1,1,2,2,3,3,4], 5),
             ([0], 1),
             ([], 0),
         ]
