b7a5bb6f290a07708b7ea055d217e4c63b12346f
rsj217 <chengcong.mjm@autonavi.com>
2022-02-14 16:54:11 +0800

    update

--- src/solution/101_symmetric_tree.py
+++ src/solution/101_symmetric_tree.py
@@ -4,16 +4,16 @@ import unittest
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        if root is None:
-            return True
-        return self._isSymmetric(root.left, root.right)
+        def dfs(left, right) -> bool:
+            if left is None and right is None:
+                return True
+            if left is None or right is None:
+                return False
+            if left.val != right.val:
+                return False
+            return dfs(left.left, right.right) and dfs(left.right, right.left)
 
-    def _isSymmetric(self, p: TreeNode, q: TreeNode) -> bool:
-        if p is None and q is None:
-            return True
-        elif p is None or q is None:
-            return False
-        return p.val == q.val and self._isSymmetric(p.left, q.right) and self._isSymmetric(p.right, q.left)
+        return dfs(root.left, root.right)
 
 
 class TestSolution(unittest.TestCase):
@@ -29,7 +29,7 @@ class TestSolution(unittest.TestCase):
         for nums, answer in self.test_case:
             root = TreeNode.create(nums)
             ans = self.s.isSymmetric(root)
-            self.assertEqual(answer,ans )
+            self.assertEqual(answer, ans)
 
 
 if __name__ == '__main__':
