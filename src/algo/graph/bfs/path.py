78df9a87099ad20be41af6cdc62899e159dfca2a
rsj217 <rsj217@gmail.com>
2023-12-24 22:52:00 +0800

    bfs algo

---
+++ src/algo/graph/bfs/path.py
@@ -0,0 +1,119 @@
+from collections import deque
+import unittest
+
+from typing import Dict, List
+from src.datastruct.adj_list_graph import AdjListGraph
+from src.algo.graph import _build_connect_graph, _build_disconnect_graph
+
+
+def single_source_path(g: AdjListGraph, s: int, t: int) -> List[int]:
+    def bfs(v: int, parent: int, prev: Dict[int, int], visited: Dict[int, int]):
+        queue = deque(maxlen=g.vsize)
+        queue.append(v)
+        visited[v] = True
+        prev[v] = parent
+        
+        while 0 < len(queue):
+            v = queue.popleft()
+            for w in g.adjs(v):
+                if not visited[w]:
+                    queue.append(w)
+                    visited[w] = True
+                    prev[w] = v
+    
+    visited = {k: False for k in g.adj}
+    prev = {k: k for k in g.adj}
+    bfs(s, s, prev, visited)
+    
+    ans = []
+    if not visited[t]:
+        return ans
+    
+    curr = t
+    while curr != s:
+        ans.append(curr)
+        curr = prev[curr]
+    ans.append(s)
+    ans.reverse()
+    return ans
+
+
+def path(g: AdjListGraph, s: int, t: int) -> List[int]:
+    """ 从 s 到 t 的路径，找到后提前终止遍历
+    """
+    
+    def bfs(v: int, parent: int, prev: Dict[int, int], visited: Dict[int, bool]) -> bool:
+        queue = deque(maxlen=g.vsize)
+        queue.append(v)
+        visited[v] = True
+        
+        while 0 < len(queue):
+            v = queue.popleft()
+            if v == t:
+                return True
+            for w in g.adjs(v):
+                if not visited[w]:
+                    queue.append(w)
+                    visited[w] = True
+                    prev[w] = v
+        
+        return False
+    
+    assert 0 <= s, "vertex invalid"
+    assert 0 <= t, "vertex invalid"
+    
+    visited = {k: False for k in g.adj}
+    prev = {k: k for k in g.adj}
+    is_find = bfs(s, s, prev, visited)
+    ans = []
+    if not is_find:
+        return ans
+    
+    curr = t
+    while curr != s:
+        ans.append(curr)
+        curr = prev[curr]
+    ans.append(s)
+    ans.reverse()
+    return ans
+
+
+class TestSingleSourcePath(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.dg = _build_disconnect_graph()
+        self.g = _build_connect_graph()
+    
+    def test_single_source_path(self):
+        self.assertEqual(single_source_path(self.g, 0, 1), [0, 1])
+        self.assertEqual(single_source_path(self.g, 0, 2), [0, 2])
+        self.assertEqual(single_source_path(self.g, 0, 3), [0, 1, 3])
+        self.assertEqual(single_source_path(self.g, 0, 4), [0, 1, 4])
+        self.assertEqual(single_source_path(self.g, 0, 5), [0, 1, 3, 5])
+        self.assertEqual(single_source_path(self.g, 0, 6), [0, 2, 6])
+        
+        self.assertEqual(single_source_path(self.dg, 0, 1), [0, 1])
+        self.assertEqual(single_source_path(self.dg, 0, 2), [0, 2])
+        self.assertEqual(single_source_path(self.dg, 0, 3), [0, 1, 3])
+        self.assertEqual(single_source_path(self.dg, 0, 4), [0, 1, 4])
+        self.assertEqual(single_source_path(self.dg, 0, 5), [])
+        self.assertEqual(single_source_path(self.dg, 0, 6), [0, 2, 6])
+    
+    def test_path(self):
+        self.assertEqual(single_source_path(self.g, 0, 1), [0, 1])
+        self.assertEqual(single_source_path(self.g, 0, 2), [0, 2])
+        self.assertEqual(single_source_path(self.g, 0, 3), [0, 1, 3])
+        self.assertEqual(single_source_path(self.g, 0, 4), [0, 1, 4])
+        self.assertEqual(single_source_path(self.g, 0, 5), [0, 1, 3, 5])
+        self.assertEqual(single_source_path(self.g, 0, 6), [0, 2, 6])
+        
+        self.assertEqual(single_source_path(self.dg, 0, 1), [0, 1])
+        self.assertEqual(single_source_path(self.dg, 0, 2), [0, 2])
+        self.assertEqual(single_source_path(self.dg, 0, 3), [0, 1, 3])
+        self.assertEqual(single_source_path(self.dg, 0, 4), [0, 1, 4])
+        self.assertEqual(single_source_path(self.dg, 0, 5), [])
+        self.assertEqual(single_source_path(self.dg, 0, 6), [0, 2, 6])
+
+
+if __name__ == '__main__':
+    unittest.main()
--- src/algo/graph/bfs/traversal.py
+++ src/algo/graph/bfs/traversal.py
@@ -25,6 +25,52 @@ def bfsorder(g: AdjListGraph) -> Generator:
             yield from bfs(k, visited)
 
 
+def levelorder(g: AdjListGraph) -> Generator:
+    def bfs(v: int, visited: Dict[int, bool]) -> Generator:
+        queue = deque(maxlen=g.vsize)
+        queue.append(v)
+        visited[v] = True
+        while 0 < len(queue):
+            lsize = len(queue)
+            level = []
+            for _ in range(lsize):
+                v = queue.popleft()
+                level.append(v)
+                for w in g.adjs(v):
+                    if not visited[w]:
+                        queue.append(w)
+                        visited[w] = True
+            yield level
+    
+    visited = {k: False for k in g.adj}
+    for k in g.adj:
+        if not visited[k]:
+            yield from bfs(k, visited)
+
+
+def disorder(g: AdjListGraph, s: int, t: int) -> int:
+    def bfs(v: int, dis: Dict[int, int], visited: Dict[int, bool]):
+        queue = deque(maxlen=g.vsize)
+        queue.append(v)
+        visited[v] = True
+        
+        while 0 < len(queue):
+            v = queue.popleft()
+            for w in g.adjs(v):
+                if not visited[w]:
+                    queue.append(w)
+                    visited[w] = True
+                    dis[w] = dis[v] + 1
+    
+    assert 0 <= s, "vertex invalid"
+    visited = {k: False for k in g.adj}
+    dis = {k: 0 for k in g.adj}
+    bfs(s, dis, visited)
+    if not visited[t]:
+        return -1
+    return dis[t]
+
+
 class TestGraphTraversal(unittest.TestCase):
     def setUp(self) -> None:
         self.dg = _build_disconnect_graph()
@@ -33,6 +79,36 @@ class TestGraphTraversal(unittest.TestCase):
     def test_bfsorder(self):
         self.assertEqual(list(bfsorder(self.g)), [0, 1, 2, 3, 4, 6, 5])
         self.assertEqual(list(bfsorder(self.dg)), [0, 1, 2, 3, 4, 6, 5])
+    
+    def test_levelorder(self):
+        self.assertEqual(list(levelorder(self.g)), [
+            [0],
+            [1, 2],
+            [3, 4, 6],
+            [5]])
+        self.assertEqual(list(levelorder(self.dg)), [
+            [0],
+            [1, 2],
+            [3, 4, 6],
+            [5]
+        ])
+    
+    def test_disorder(self):
+        self.assertEqual(disorder(self.g, 0, 0), 0)
+        self.assertEqual(disorder(self.g, 0, 1), 1)
+        self.assertEqual(disorder(self.g, 0, 2), 1)
+        self.assertEqual(disorder(self.g, 0, 3), 2)
+        self.assertEqual(disorder(self.g, 0, 4), 2)
+        self.assertEqual(disorder(self.g, 0, 5), 3)
+        self.assertEqual(disorder(self.g, 0, 6), 2)
+        
+        self.assertEqual(disorder(self.dg, 0, 0), 0)
+        self.assertEqual(disorder(self.dg, 0, 1), 1)
+        self.assertEqual(disorder(self.dg, 0, 2), 1)
+        self.assertEqual(disorder(self.dg, 0, 3), 2)
+        self.assertEqual(disorder(self.dg, 0, 4), 2)
+        self.assertEqual(disorder(self.dg, 0, 5), -1)
+        self.assertEqual(disorder(self.dg, 0, 6), 2)
 
 
 if __name__ == '__main__':
