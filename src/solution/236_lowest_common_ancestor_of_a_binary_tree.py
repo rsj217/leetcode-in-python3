56b07544d0b4630db54f5c773ef6cc34c85712a7
rsj217 <rsj217@gmail.com>
2024-01-14 10:57:09 +0800

    二叉树最近公共主先

--- src/solution/236_lowest_common_ancestor_of_a_binary_tree.py
+++ src/solution/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -1,28 +1,33 @@
 from src.datastruct.treenode import TreeNode
+from typing import Optional
 import unittest
 
 
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        if root is None:
-            return
-
-        if root.val == p.val or root.val == q.val:
-            return root
-
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-
-        if left is not None and right is not None:
-            return root
-        if left is None:
-            return right
-        if right is None:
-            return left
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> Optional[TreeNode]:
+        def dfs(root: TreeNode, p: TreeNode, q: TreeNode) -> Optional[TreeNode]:
+            if root is None:
+                return
+            if root.val == p.val or root.val == q.val:
+                return root
+            
+            left = dfs(root.left, p, q)
+            right = dfs(root.right, p, q)
+            
+            if left is not None and right is not None:
+                return root
+            elif left is not None:
+                return left
+            elif right is not None:
+                return right
+            else:
+                return None
+        
+        return dfs(root, p, q)
 
 
 class TestSolution(unittest.TestCase):
-
+    
     def setUp(self):
         self.test_case = [
             ([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4], 5, 1, 3),
@@ -30,14 +35,14 @@ class TestSolution(unittest.TestCase):
             ([1, 2], 1, 2, 1),
         ]
         self.s = Solution()
-
+    
     def test_solution(self):
         for nums, p, q, answer in self.test_case:
             root = TreeNode.deserialize(nums)
             p = TreeNode(p)
             q = TreeNode(q)
             ans = self.s.lowestCommonAncestor(root, p, q)
-            self.assertEqual(answer,ans .val)
+            self.assertEqual(answer, ans.val)
 
 
 if __name__ == '__main__':
